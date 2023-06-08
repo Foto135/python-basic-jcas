15e85d78d9515d07d91fc9d20b855febcccd620b
Foto135 <32173087+Foto135@users.noreply.github.com>
2023-06-08 09:43:48 -0500

    Delete tema_2 directory

--- tema_2/03_strings_estraer_trozos.py
+++
@@ -1,6 +0,0 @@
-texto = "Agua pasada no vuelve al molino"
-
-#str[inicio:final:saltos]
-print(texto[:])
-print(texto[3:])
-print(texto[5:11]) #recordemos que end, nos devuelve end-1
--- tema_2/08_rremplazar_textos.py
+++
@@ -1,6 +0,0 @@
-texto = "Hoy es lunes"
-print(texto)
-texto_a_reemplazar = "lunes"
-texto_unevo = "Martes"
-reemplazo = texto.replace(texto_a_reemplazar, texto_unevo)
-print(reemplazo)
